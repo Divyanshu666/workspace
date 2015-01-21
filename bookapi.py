2d878b87d03e5983ebccec7b3f12da110fbef269
divyanshutyagi10@gmail.com <divyanshutyagi10@gmail.com>
2015-01-21 13:56:08 +0530

    manual book search and book search on the basis of trendy hashtag from twitter implemented

--- bookapi.py
+++ bookapi.py
@@ -1,68 +1,112 @@
 import json
 import urllib
 import requests
-import django.utils.simplejson as json
 import twitter
 import googlemaps
 import tweepy
 import re
 
+
+def search():
+	key = 'AIzaSyBH6TFovmncqKM1mwDJ6lUE7L2oqRAL8dM'
+
+	query = raw_input("Enter query : ")
+	# query_search = urllib.urlencode({query})
+	libraryRestrict = raw_input("Restrict search : ") 
+	orderBy = raw_input("orderBy (relevance/newest) : ")
+	printType = raw_input("PrintType(books/magazines) : ")
+	maxResults = raw_input("Results (max 40) : ")
+
+	url ='https://www.googleapis.com/books/v1/volumes?q=%s&key=%s&libraryRestrict=no-restrict&orderBy=relevance&printType=books&maxResults=40' %(query,key)
+
+	search_url = urllib.urlopen(url)
+	results  = json.loads(search_url.read())
+	# print results
+	for res in results['items']:
+		if 'authors' in res['volumeInfo']:
+			# data = [dict() for x in range(10)]
+			# for dictlist in data
+			data= {
+					'book_title':res['volumeInfo']['title'].encode('utf-8') ,
+					'author':res['volumeInfo']['authors'][0]
+				}
+		print data
+	# if "items" in results.keys(): 
+	# 	for res in results['items']:
+	# 		if 'authors' in res['volumeInfo']:
+	# 			# print '\t' + '\t' + '\t' + res['volumeInfo']['authors'][0]
+	# 			data= {
+	# 					'book_title':res['volumeInfo']['title'].encode('utf-8') ,
+	# 					'author':res['volumeInfo']['authors'][0]
+	# 				}
+	# 		else:
+	# 			data= {
+	# 					'book_title':res['volumeInfo']['title'].encode('utf-8'),
+	# 				}
+	# 	print data
+	# 	return data
+
 def twitter_trend_hashtag():
 
 	CONSUMER_KEY = 'bhipusATdYgFpi9W2nCaHK57c'
 	CONSUMER_SECRET = 'XwTylNDMSogXIMhKEwc5uaSPTcyYkTDbfHj6ZifWxZu3oLl0RJ'
-
 	ACCESS_TOKEN = '994668080-T9nQ2EH6LBZFeND2HKJ0mfcj0Zm5lUZ57uSNQ0Hx'
 	ACCESS_SECRET = '72ppPOwj3vZtetdZXTLm8YSv3ZCCId0w1rEIQloBR8VQx'
 
 	# url = 'https://api.twitter.com/1.1/trends/place.json?id=1'
+
 	twitter_api = twitter.Api(consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET,access_token_key=ACCESS_TOKEN,access_token_secret=ACCESS_SECRET)
-	
 	twitter_trends = twitter_api.GetTrendsCurrent()
 	for trend in twitter_trends:
 		# print trend.name
 		hashtag = trend.name
-		Search(hashtag.replace('#',''))
+		hashtag = hashtag.replace('#','').encode('utf-8')
+		# print hashtag
+		google_book_search(hashtag)
 
-def tweepy_trend_hashtag():
-	auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
-
-	api = tweepy.API(auth)
-	trends1 = api.trends_place(1)
-
-	hashtags = [x['name'] for x in trends1[0]['trends']]
-
-	for tags in hashtags:
-		Search(re.sub("#","",tags))
-
-def Search(query):
-	key = 'AIzaSyDxHeNi6StT62JnwaKh36LFqSnN-VMpUL8'
-	# query = raw_input("Enter query : ")
-	# query_search = urllib.urlencode({query})
-	# libraryRestrict = raw_input("Restrict search : ") 
-	# orderBy = raw_input("orderBy (relevance/newest)")
-	# printType = raw_input("PrintType(books/magazines) : ")
-	# maxResults = raw_input("Results (max 40) : ")
-
-
-	# url ='https://www.googleapis.com/books/v1/volumes?q=%s&key=%s&libraryRestrict=no-restrict&orderBy=relevance&printType=all&maxResults=40' %(query,key,libraryRestrict,orderBy,printType,maxResults)
+# def tweepy_trend_hashtag():
+# 	CONSUMER_KEY = 'bhipusATdYgFpi9W2nCaHK57c'
+# 	CONSUMER_SECRET = 'XwTylNDMSogXIMhKEwc5uaSPTcyYkTDbfHj6ZifWxZu3oLl0RJ'
+# 	ACCESS_TOKEN = '994668080-T9nQ2EH6LBZFeND2HKJ0mfcj0Zm5lUZ57uSNQ0Hx'
+# 	ACCESS_SECRET = '72ppPOwj3vZtetdZXTLm8YSv3ZCCId0w1rEIQloBR8VQx'
 	
-	url ='https://www.googleapis.com/books/v1/volumes?q=%s&key=%s&libraryRestrict=no-restrict&orderBy=relevance&printType=all&maxResults=40' %(query,key)
+# 	auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET)
+# 	auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
+# 	api = tweepy.API(auth)
+# 	tweepy_trends = api.trends_place(1)
+# 	tweepy_hashtags = [x['name'] for x in tweepy_trends[0]['trends']]
+# 	for tags in tweepy_hashtags:
+# 		tags = re.sub("#","",tags)
+# 		# print tags
+# 		google_book_search(re.sub("#","",tags))
+
+# def unique_hashtags():
+
+
+def google_book_search(query):
+	key = 'AIzaSyBH6TFovmncqKM1mwDJ6lUE7L2oqRAL8dM'
+
+	url ='https://www.googleapis.com/books/v1/volumes?q=%s&key=%s&libraryRestrict=no-restrict&orderBy=relevance&printType=books&maxResults=40' %(query,key)
 	# search_results = urllib.urlopen(url).read()
 	search_url = urllib.urlopen(url)
-
 	results  = json.loads(search_url.read())
-	print results
-	# for res in results['items']:
-	# 	print res['volumeInfo']['title'] + " : "
-	# 	# if 'industryIdentifiers' in res['volumeInfo']:
-	# 	# 	if res['volumeInfo']['industryIdentifiers'][0]['type'] == "ISBN_13"
-	# 	# 	print '\t' + '\t' + '\t' + ""res['volumeInfo']['industryIdentifiers'][0]['identifier']
-	# 	# else:
-	# 	# 	print
-	# 	# print '\t' + res['volumeInfo']['averageRating']
-	# 	if 'authors' in res['volumeInfo']:
-	# 		print '\t' + '\t' + '\t' + res['volumeInfo']['authors'][0]
-	# 	else:
-	# 		pass
+	# print results
+	if "items" in results.keys(): 
+		for res in results['items']:
+			if 'authors' in res['volumeInfo']:
+				# print '\t' + '\t' + '\t' + res['volumeInfo']['authors'][0]
+				data= {
+						'book_title':res['volumeInfo']['title'].encode('utf-8') ,
+						'author':res['volumeInfo']['authors'][0]
+					}
+			else:
+				data= {
+						'book_title':res['volumeInfo']['title'].encode('utf-8'),
+					}
+		print data
+		return data
+
+if __name__ == "__main__":
+	twitter_trend_hashtag()
+	# tweepy_trend_hashtag()
+	search()
